c97643966fc49cea7a1ba65ef75590fbf88c57c2
unknown <>
2016-09-11 16:45:40 +0800

    if elif

--- hello.py
+++ hello.py
@@ -1,3 +1,14 @@
-print("hello world")
-a=input("what is your name")
-print("welcome",a)
+#-*-coding:utf-8-*-
+height=1.75
+weight=80.5
+bmi=weight/height/height
+if bmi<18.5:
+    print("too light")
+elif bmi <23:
+    print("normal")
+elif bmi<28:
+    print("a bit heavy")
+elif bmi<32:
+    print("obesity")
+else:
+    print("too heavy")
